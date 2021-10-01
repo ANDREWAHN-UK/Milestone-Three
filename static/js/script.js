42aaa6b30a4f0e281c252efa932beb4b342552ae
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-10-01 12:46:16 +0000

    script.js - functionality for registration items

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 
 // base page
 
+// this targets the circle, top screenLeft, and when show-nav is activated, 
+// it shows the navbar, and moves the page, which is referred to as container-base
+
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container-base')
@@ -12,18 +15,49 @@ close.addEventListener('click', () => container.classList.remove('show-nav'))
 
 // registration page:
 
-const signUpBtn = document.querySelector(".signup-btn");
-const signInBtn = document.querySelector(".signin-btn");
-const formsWrapper = document.querySelector(".forms-wrapper");
+let id = (id) => document.getElementById(id);
 
+let classes = (classes) => document.getElementsByClassName(classes);
 
-// when clicked, adds or removes the change class
-signUpBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formsWrapper.classList.add("change");
-});
+// tells js what ids to target
+let username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
 
-signInBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formsWrapper.classList.remove("change");
-});
+  // tells js to watch out for the submit action, and react accordingly
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    registration(username, 0, "Username cannot be blank");
+    registration(email, 1, "Email cannot be blank");
+    registration(password, 2, "Password cannot be blank");
+  });
+
+// id refers to our ids 
+// serial targets the classes 
+// message prints a message
+  let registration = (id, serial, message) => {
+    // trim removes white space in the user input fields
+    if (id.value.trim() === "") {
+      errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+    
+    // if no user input, displays failure icon
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+    } 
+    
+    else {
+      errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+    
+    // if there is user input, displays correct icon
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+    }
+  }
