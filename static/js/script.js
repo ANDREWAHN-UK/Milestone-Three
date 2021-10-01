42cd4506b923a95a22e8e4c896c5465e50908c95
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-10-01 09:09:56 +0000

    script.js - updated functionality for base.html, added functionality for registration page

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,29 @@
 
+// base page
+
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const container = document.querySelector('.container-base')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+// registration page:
+
+const signUpBtn = document.querySelector(".signup-btn");
+const signInBtn = document.querySelector(".signin-btn");
+const formsWrapper = document.querySelector(".forms-wrapper");
+
+
+// when clicked, adds or removes the change class
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formsWrapper.classList.add("change");
+});
+
+signInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formsWrapper.classList.remove("change");
+});
