04b63f0acca2105924a7b1258a91c999df21dee5
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-10-10 12:45:42 +0000

    templates - adjusted elements for jinja for loop; static - added carouserl functioin, updated images, updated styling

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,24 @@ open.addEventListener('click', () => container.classList.add('show-nav'))
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
