ba1b3e5448fb52b476fe69dcefd0c8e7831ea79d
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-10-21 11:11:47 +0000

    static - changed script.js in accordance with the tester recommendations; style.css -added a pulse animation to .circle + wrote comments

--- static/js/script.js
+++ static/js/script.js
@@ -4,13 +4,13 @@
 // this targets the circle, top screenLeft, and when show-nav is activated, 
 // it shows the navbar, and moves the page, which is referred to as container-base
 
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container-base')
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container-base');
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => container.classList.add('show-nav'));
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'));
 
 
 const swiper = new Swiper('.swiper', {
